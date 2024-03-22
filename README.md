2a9dd4d78a00b36e7179b8547f91a1f9b40e968e
sharvari2 <84336847+sharvari2@users.noreply.github.com>
2024-03-22 06:41:55 +0000

    Added stop and test scripts

---
+++ README.md
@@ -0,0 +1,256 @@
+# Microgrid
+Hyperledger network for microgrids
+
+
+## Basic system setup
+
+Assumes Ubuntu
+
+### Check docker version
+```bash
+docker --version
+```
+Docker compose version greater than 1.14.0 is required
+```bash
+docker-compose --version
+```
+
+### Check GoLang
+Check
+```bash
+go version
+```
+
+### Add go location to ```GOPATH``` and ```GOPATH``` to ```PATH```
+Add path to ```.profile```
+```bash
+echo 'export GOPATH=/usr/local/go' >> ~/.profile
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
+source ~/.profile
+```
+
+### Download Hyperledger fabric-samples to preffered directory
+```bash
+git clone -b master https://github.com/hyperledger/fabric-samples.git
+```
+
+### Download and install fabric binaries version 1.2 into current directory
+```bash
+curl -sSL http://bit.ly/2ysbOFE | bash -s 1.2.0
+```
+
+## Set up the network
+
+### Quick
+Configure
+```bash
+bash configure.sh
+```
+Start
+```bash
+bash start.sh
+```
+
+Stop
+```bash
+bash stop.sh
+```
+
+### Step-by-step
+
+```bash
+$ ../bin/cryptogen generate --config crypto-config.yaml --output=crypto-config
+```
+
+These lines should be printed:
+```bash
+house01.microgrid.org
+house02.microgrid.org
+house03.microgrid.org
+house04.microgrid.org
+house05.microgrid.org
+house06.microgrid.org
+```
+
+and a directory called ```crypto-config``` should created.
+
+#### Create the orderer genesis block, channel configuration transaction and the anchor peer transactions, one for each peer organisation
+
+The ```configtxgen``` command doesn't automatically create the ```channel-artifacts``` directory.
+Do it manually.
+```bash
+$ mkdir channel-artifacts
+```
+
+##### Genesis block:
+```bash
+$ ../bin/configtxgen -profile OrdererGenesis -outputBlock ./channel-artifacts/genesis.block
+```
+
+The last line printed should look something like:
+```bash
+2018-08-21 17:26:31.191 EDT [common/tools/configtxgen] doOutputBlock -> INFO 013 Writing genesis block
+```
+
+##### Channel configuration transaction:
+```bash
+$ ../bin/configtxgen -profile degrid -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID degrid
+```
+```channel.tx``` should be created in ```channel-artifacts``` and the last line printed should look something like this:
+```
+2018-08-24 14:38:49.079 EDT [common/tools/configtxgen] doOutputChannelCreateTx -> INFO 010 Writing new channel tx
+```
+
+##### Anchor peer transactions:
+```bash
+$ ../bin/configtxgen -profile degrid -outputAnchorPeersUpdate ./channel-artifacts/House01Anchor.tx -channelID degrid -asOrg House01MSP
+
+$ ../bin/configtxgen -profile degrid -outputAnchorPeersUpdate ./channel-artifacts/House02Anchor.tx -channelID degrid -asOrg House02MSP
+
+$ ../bin/configtxgen -profile degrid -outputAnchorPeersUpdate ./channel-artifacts/House03Anchor.tx -channelID degrid -asOrg House03MSP
+
+$ ../bin/configtxgen -profile degrid -outputAnchorPeersUpdate ./channel-artifacts/House04Anchor.tx -channelID degrid -asOrg House04MSP
+
+$ ../bin/configtxgen -profile degrid -outputAnchorPeersUpdate ./channel-artifacts/House05Anchor.tx -channelID degrid -asOrg House05MSP
+
+$ ../bin/configtxgen -profile degrid -outputAnchorPeersUpdate ./channel-artifacts/House06Anchor.tx -channelID degrid -asOrg House06MSP
+```
+
+```House01Anchor.tx```, ```House02Anchor.tx``` ... should be located in ```channel-artifacts```
+and the last lines printed should look something like this:
+```
+2018-08-24 14:46:37.491 EDT [common/tools/configtxgen] main -> INFO 001 Loading configuration
+2018-08-24 14:46:37.499 EDT [common/tools/configtxgen] doOutputAnchorPeersUpdate -> INFO 002 Generating anchor peer update
+2018-08-24 14:46:37.499 EDT [common/tools/configtxgen] doOutputAnchorPeersUpdate -> INFO 003 Writing anchor peer update
+```
+#### Peer-base and docker-compose
+
+```bash
+$ docker-compose -f docker-compose-cli.yaml up -d
+```
+
+The last few lines printed should look something like this:
+```bash
+dde25187799d: Pull complete
+Digest: sha256:24cca44a2f2ab6325c6ccc1c91a10bd3e0e71764037a85a473f7e9621b3a0f91
+Status: Downloaded newer image for hyperledger/fabric-tools:latest
+Creating peer1.house03.microgrid.org ... done
+Creating peer0.house01.microgrid.org ... done
+Creating peer1.house02.microgrid.org ... done
+Creating peer0.house02.microgrid.org ... done
+Creating peer1.house06.microgrid.org ... done
+Creating peer1.house01.microgrid.org ... done
+Creating peer1.house04.microgrid.org ... done
+Creating peer0.house04.microgrid.org ... done
+Creating peer0.house05.microgrid.org ... done
+Creating peer0.house06.microgrid.org ... done
+Creating orderer.microgrid.org       ... done
+Creating peer0.house03.microgrid.org ... done
+Creating peer1.house05.microgrid.org ... done
+Creating cli                         ... done
+Creating chaincode                   ... done
+```
+Start docker cli:
+```bash
+docker start cli
+```
+Enter the ```cli``` docker container:
+```bash
+docker exec -it cli bash
+```
+Should look something like this:
+
+```
+root@b411f1d6d95e:/opt/gopath/src/github.com/hyperledger/fabric/peer#
+```
+
+#### Stop network and remove docker images
+```bash
+docker-compose -f docker-compose-cli.yaml down --volumes
+docker rm -f $(docker ps -aq)
+docker rmi -f $(docker images -q)
+```
+
+
+## Create the channel
+
+First enter the cli:
+```
+docker exec -it cli bash # Enter the cli
+```
+and then create the channel:
+```bash
+export CHANNEL_NAME=degrid
+peer channel create -o orderer.microgrid.org:7050 -c $CHANNEL_NAME -f ./channel-artifacts/channel.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/microgrid.org/orderers/orderer.microgrid.org/msp/tlscacerts/tlsca.microgrid.org-cert.pem
+```
+Alternatively, without entering the cli
+```bash
+docker exec -ti cli sh -c "peer channel create -o orderer.microgrid.org:7050 -c degrid -f ./channel-artifacts/channel.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/microgrid.org/orderers/orderer.microgrid.org/msp/tlscacerts/tlsca.microgrid.org-cert.pem"
+```
+
+## Join peer to the channel
+
+Join peer0.house01.microgrid.org to the channel
+
+From the cli:
+```bash
+CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/house01.microgrid.org/users/Admin@house01.microgrid.org/msp CORE_PEER_ADDRESS=peer0.house01.microgrid.org:7051 CORE_PEER_LOCALMSPID="House01MSP" CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/house01.microgrid.org/peers/peer0.house01.microgrid.org/tls/ca.crt peer channel join -b degrid.block
+
+CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/house01.microgrid.org/users/Admin@house01.microgrid.org/msp CORE_PEER_ADDRESS=peer1.house01.microgrid.org:7051 CORE_PEER_LOCALMSPID="House01MSP" CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/house01.microgrid.org/peers/peer1.house01.microgrid.org/tls/ca.crt peer channel join -b degrid.block
+```
+
+Without entering the cli:
+```bash
+docker exec -ti cli sh -c 'CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/house01.microgrid.org/users/Admin@house01.microgrid.org/msp CORE_PEER_ADDRESS=peer0.house01.microgrid.org:7051 CORE_PEER_LOCALMSPID="House01MSP"
+CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/house01.microgrid.org/peers/peer0.house01.microgrid.org/tls/ca.crt peer channel join -b degrid.block'
+```
+
+## Chaincode
+
+### Quick
+```
+bash setup_chaincode.sh
+```
+
+### Step-by-step
+
+Assumes you are in ```microgrid```
+```bash
+$ cd chaincode/go/src
+```
+Test compile the chaincode outside of a container
+```
+$ go get -u github.com/hyperledger/fabric-chaincode-go/shim
+$ go build
+```
+
+Install the chaincode
+```bash
+CC_SRC_PATH=chaincode/go/src
+docker exec -e "CORE_PEER_LOCALMSPID=House01MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/house01.microgrid.org/users/Admin@house01.microgrid.org/msp" cli peer chaincode install -n carecords -v 1.0 -p "$CC_SRC_PATH" -l golang
+```
+
+The last lines printed should be something like this:
+```bash
+2018-09-05 20:48:05.131 UTC [container] WriteFileToPackage -> DEBU 04e Writing file to tarball: src/chaincode/go/src/carecords.go
+2018-09-05 20:48:05.135 UTC [msp/identity] Sign -> DEBU 04f Sign: plaintext: 0ACA070A5B08031A0B088582C1DC0510...F7C67F020000FFFF6A084374001A0000
+2018-09-05 20:48:05.135 UTC [msp/identity] Sign -> DEBU 050 Sign: digest: 88E3BE1783D75457E36882431DCA4EDC77A7996F0C0BD3524E8DB208387A9B0B
+2018-09-05 20:48:05.137 UTC [chaincodeCmd] install -> INFO 051 Installed remotely response:<status:200 payload:"OK" >
+```
+
+
+Instantiate the chaincode in the channel (degrid)
+```bash
+docker exec -ti cli sh -c "peer chaincode instantiate -o orderer.microgrid.org:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/microgrid.org/orderers/orderer.microgrid.org/msp/tlscacerts/tlsca.microgrid.org-cert.pem -C degrid -n carecords -v 1.0 -c '{\"Args\":[\"init\",\"a\", \"100\", \"b\",\"200\"]}' -P \"OR ('House01MSP.member','House02MSP.member')\""
+```
+## Test Chaincode
+Test Carecords
+```bash
+bash test_carecords.sh
+```
+Test MSP
+```
+bash test_msp.sh
+```
+
+## Application
---
+++ test_mcp.sh
@@ -0,0 +1,164 @@
+
+CONTRACT_ID="mcp"
+CHANNEL_ID="degrid"
+ORDERER_ADDR="orderer.microgrid.org"
+UNIT="Wh"
+LOG_FILE="mcp_test.log"
+CA_FILE="/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/microgrid.org/orderers/$ORDERER_ADDR/msp/tlscacerts/tlsca.microgrid.org-cert.pem"
+AGENTTYPES_SUPPLY=("PV" "PV" "PV" "PV" "PV" "PV")
+AGENTTYPES_DEMAND=("EV" "BESS" "HVAC" "EV" "BESS" "HVAC")
+AGENTS=("House01" "House02" "House03" "House04" "House05" "House06")
+
+AGENT1=${AGENTS[0]}
+AGENT1_ADDR=${AGENT1,,}".microgrid.org" # House01 to lower case
+PEER1="peer0."${AGENT1,,}".microgrid.org"
+TLS_ROOT_CERT_FILE_AGENT1="/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/$AGENT1_ADDR/peers/$PEER1/tls/ca.crt"
+
+PEER2="peer0."${AGENTS[1],,}".microgrid.org"
+AGENT2_ADDR=${AGENTS[1],,}".microgrid.org" # House02 to lower case
+TLS_ROOT_CERT_FILE_AGENT2="/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/$AGENT2_ADDR/peers/$PEER2/tls/ca.crt"
+
+echo "A log of the test will be written to $PWD/$LOG_FILE"
+
+#-------------------------------------------------------------------------------
+# Running random bids
+#-------------------------------------------------------------------------------
+DATE_TIME=$( date +"%Y-%m-%d %T" )
+echo -e "    Putting random bids in putBid()\n========================================\n Time: $DATE_TIME" > $LOG_FILE
+echo "Running random bids"
+
+for i in {1..4}
+do
+  for ((j=0; j<${#AGENTS[@]}; j++))
+  do
+    AGENT=${AGENTS[j]}
+    PEER="peer0."${AGENT,,}".microgrid.org"
+    AGENT_ADDR=${AGENT,,}".microgrid.org" # to lower case
+    TLS_ROOT_CERT_FILE_AGENT="/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/$AGENT_ADDR/peers/$PEER/tls/ca.crt"
+
+    PRICE=$(( ( RANDOM % 100 )  + 1 ))
+    QUANTITY=$(( ( RANDOM % 100 )  - 50 ))
+    if ((QUANTITY < 0)); then
+      AGENTTYPE="${AGENTTYPES_SUPPLY[$j]}"
+    else
+      AGENTTYPE="${AGENTTYPES_DEMAND[$j]}"
+    fi
+    echo -e "\nInvoking $CONTRACT_ID with arguments:\n func: putBid\n\tprice:$PRICE\n\tquantity:$QUANTITY\n\taguent:$AGENT\n\tagent type:$AGENTTYPE\n\tunit:$UNIT"  >> $LOG_FILE
+    docker exec -ti cli sh -c  "peer chaincode invoke -o $ORDERER_ADDR:7050 --tls true --cafile $CA_FILE -C $CHANNEL_ID -n $CONTRACT_ID --peerAddresses $PEER1:7051 --tlsRootCertFiles $TLS_ROOT_CERT_FILE_AGENT1 --peerAddresses $PEER:7051 --tlsRootCertFiles $TLS_ROOT_CERT_FILE_AGENT  -c '{\"Args\":[\"putBid\",\"$PRICE\",\"$QUANTITY\",\"$AGENT\",\"$AGENTTYPE\",\"$UNIT\"]}'"   >> $LOG_FILE
+  done
+  sleep 30
+done
+sleep 30
+echo -e "\ngetBidsForLastNSeconds:" >> $LOG_FILE
+docker exec -ti cli sh -c  "peer chaincode query -C $CHANNEL_ID -n $CONTRACT_ID -c '{\"Args\": [\"getBidsForLastNSeconds\",\"300\"]}'"  >> $LOG_FILE
+echo -e "\ngetMCP:"  >> $LOG_FILE
+docker exec -ti cli sh -c  "peer chaincode query -C $CHANNEL_ID -n $CONTRACT_ID -c '{\"Args\": [\"getMCP\",\"300\"]}'"  >> $LOG_FILE
+
+
+#-------------------------------------------------------------------------------
+# Running tests from 'Intra-community Real-time Marketplace'
+#-------------------------------------------------------------------------------
+echo -e "\n\n Running tests from 'Intra-community Real-time Marketplace' (MCP Calculation Procedure.pdf)\n============================================================================================"  >> $LOG_FILE
+echo "Runnng test cases from 'Intra-community Real-time Marketplace'"
+
+# Function for running the tests
+function run_test {
+  sleep 300
+  echo -e "\tTest $TEST"
+  DATE_TIME=$( date +"%Y-%m-%d %T" )
+  echo -e "\n Test $TEST\n-------------\nStart time $DATE_TIME" >> $LOG_FILE
+  for ((i=0; i<${#PRICES[@]}; i++))
+  do
+    echo -e "\nInvoking $CONTRACT_ID with arguments:\n func: putBid\n\tprice:${PRICES[$i]}\n\tquantity:${QUANTITES[$i]}\n\taguent:${AGENTIDS[$i]}\n\tagent type:${AGENTTYPES[i]}\n\tunit:$UNIT" >> $LOG_FILE
+    docker exec -ti cli sh -c  "peer chaincode invoke -o $ORDERER_ADDR:7050 --tls true --cafile $CA_FILE -C $CHANNEL_ID -n $CONTRACT_ID --peerAddresses $PEER1:7051 --tlsRootCertFiles $TLS_ROOT_CERT_FILE_AGENT1 --peerAddresses $PEER2:7051 --tlsRootCertFiles $TLS_ROOT_CERT_FILE_AGENT2  -c '{\"Args\":[\"putBid\",\"${PRICES[i]}\",\"${QUANTITES[i]}\",\"${AGENTIDS[i]}\",\"${AGENTTYPE[i]}\",\"$UNIT\"]}'" >> $LOG_FILE
+    sleep 30
+  done
+  sleep 30
+  DATE_TIME=$( date +"%Y-%m-%d %T" )
+  echo -e "\ngetBidsForLastNSeconds ($DATE_TIME):" >> $LOG_FILE
+  docker exec -ti cli sh -c  "peer chaincode query -C $CHANNEL_ID -n $CONTRACT_ID -c '{\"Args\": [\"getBidsForLastNSeconds\",\"300\"]}'"  >> $LOG_FILE
+  DATE_TIME=$( date +"%Y-%m-%d %T" )
+  echo -e "\ngetMCP ($DATE_TIME):" >> $LOG_FILE
+  docker exec -ti cli sh -c  "peer chaincode query -C $CHANNEL_ID -n $CONTRACT_ID -c '{\"Args\": [\"getMCP\",\"300\"]}'"  >> $LOG_FILE
+  echo -e "Correct values:\n{\"MCP\": ${CORRECT[0]}, \"BPP\": ${CORRECT[1]}}\n" >> $LOG_FILE
+}
+
+TEST=1
+PRICES=(150 100 40 50 70 120)
+QUANTITES=(400 600 500 -600 -600 -500)
+AGENTIDS=("1002003" "1004002" "1001002" "1002006" "1003001" "1003001")
+AGENTTYPES=("PV" "PV" "PV" "PV" "PV" "PV")
+CORRECT=(70 100)
+run_test
+
+TEST=2
+PRICES=(150 100 40 50 70 120)
+QUANTITES=(400 800 300 -600 -400 -700)
+AGENTIDS=("1002003" "1004002" "1001002" "1002006" "1003001" "1003001")
+AGENTTYPES=("PV" "PV" "PV" "PV" "PV" "PV")
+CORRECT=(50 150)
+run_test
+
+TEST=3
+PRICES=(150 100 40 50 70 120)
+QUANTITES=(400 800 300 -600 -600 -500)
+AGENTIDS=("1002003" "1004002" "1001002" "1002006" "1003001" "1003001")
+AGENTTYPES=("PV" "PV" "PV" "PV" "PV" "PV")
+CORRECT=(70 100)
+run_test
+
+TEST=4
+PRICES=(100 50 120 160)
+QUANTITES=(400 800 -600 -400)
+AGENTIDS=("1002003" "1004002" "1002006" "1003001")
+AGENTTYPES=("PV" "PV" "PV" "PV")
+CORRECT=(0 0)
+run_test
+
+
+docker exec -ti cli sh -c  "peer chaincode invoke -o $ORDERER_ADDR:7050 --tls true --cafile $CA_FILE -C $CHANNEL_ID -n $CONTRACT_ID --peerAddresses $PEER1:7051 --tlsRootCertFiles $TLS_ROOT_CERT_FILES_HOUSE1 --peerAddresses $PEER2:7051 --tlsRootCertFiles $TLS_ROOT_CERT_FILES_HOUSE2  -c '{\"Args\":[\"appendRecord\",\"House01_001\",\"2019-01-25 12:03\",\"10\"]}'"
+
+sleep 360
+
+
+PRICE=50
+QUANTITY=-120
+AGENTID="1002003"
+AGENTTYPE="PV"
+echo -e "Invoking $CONTRACT_ID with arguments:\n func: putBid\n\tprice:$PRICE\n\tquantity:$QUANTITY\n\taguent:$AGENTID\n\tagent type:$AGENTTYPE\n\tunit:$UNIT" >> $LOG_FILE
+docker exec -ti cli sh -c  "peer chaincode invoke -o $ORDERER_ADDR:7050 --tls true --cafile $CA_FILE -C $CHANNEL_ID -n $CONTRACT_ID --peerAddresses $PEER1:7051 --tlsRootCertFiles $TLS_ROOT_CERT_FILES_HOUSE1 --peerAddresses $PEER2:7051 --tlsRootCertFiles $TLS_ROOT_CERT_FILES_HOUSE2  -c '{\"Args\":[\"putBid\",\"$PRICE\",\"$QUANTITY\",\"$AGENTID\",\"$AGENTTYPE\",\"$UNIT\"]}'" >> $LOG_FILE
+
+PRICE=100
+QUANTITY=600
+AGENTID="1004002"
+AGENTTYPE="EV"
+echo -e "Invoking $CONTRACT_ID with arguments:\n func: putBid\n\tprice:$PRICE\n\tquantity:$QUANTITY\n\taguent:$AGENTID\n\tagent type:$AGENTTYPE\n\tunit:$UNIT" >> $LOG_FILE
+docker exec -ti cli sh -c  "peer chaincode invoke -o $ORDERER_ADDR:7050 --tls true --cafile $CA_FILE -C $CHANNEL_ID -n $CONTRACT_ID --peerAddresses $PEER1:7051 --tlsRootCertFiles $TLS_ROOT_CERT_FILES_HOUSE1 --peerAddresses $PEER2:7051 --tlsRootCertFiles $TLS_ROOT_CERT_FILES_HOUSE2  -c '{\"Args\":[\"putBid\",\"$PRICE\",\"$QUANTITY\",\"$AGENTID\",\"$AGENTTYPE\",\"$UNIT\"]}'" >> $LOG_FILE
+
+PRICE=150
+QUANTITY=400
+AGENTID="1001002"
+AGENTTYPE="HVAC"
+echo -e "Invoking $CONTRACT_ID with arguments:\n func: putBid\n\tprice:$PRICE\n\tquantity:$QUANTITY\n\taguent:$AGENTID\n\tagent type:$AGENTTYPE\n\tunit:$UNIT" >> $LOG_FILE
+docker exec -ti cli sh -c  "peer chaincode invoke -o $ORDERER_ADDR:7050 --tls true --cafile $CA_FILE -C $CHANNEL_ID -n $CONTRACT_ID --peerAddresses $PEER1:7051 --tlsRootCertFiles $TLS_ROOT_CERT_FILES_HOUSE1 --peerAddresses $PEER2:7051 --tlsRootCertFiles $TLS_ROOT_CERT_FILES_HOUSE2  -c '{\"Args\":[\"putBid\",\"$PRICE\",\"$QUANTITY\",\"$AGENTID\",\"$AGENTTYPE\",\"$UNIT\"]}'" >> $LOG_FILE
+
+PRICE=100
+QUANTITY=-1000
+AGENTID="1002006"
+AGENTTYPE="PV"
+echo -e "Invoking $CONTRACT_ID with arguments:\n func: putBid\n\tprice:$PRICE\n\tquantity:$QUANTITY\n\taguent:$AGENTID\n\tagent type:$AGENTTYPE\n\tunit:$UNIT" >> $LOG_FILE
+docker exec -ti cli sh -c  "peer chaincode invoke -o $ORDERER_ADDR:7050 --tls true --cafile $CA_FILE -C $CHANNEL_ID -n $CONTRACT_ID --peerAddresses $PEER1:7051 --tlsRootCertFiles $TLS_ROOT_CERT_FILES_HOUSE1 --peerAddresses $PEER2:7051 --tlsRootCertFiles $TLS_ROOT_CERT_FILES_HOUSE2  -c '{\"Args\":[\"putBid\",\"$PRICE\",\"$QUANTITY\",\"$AGENTID\",\"$AGENTTYPE\",\"$UNIT\"]}'" >> $LOG_FILE
+
+PRICE=40
+QUANTITY=500
+AGENTID="1003001"
+AGENTTYPE="PV"
+echo -e "Invoking $CONTRACT_ID with arguments:\n func: putBid\n\tprice:$PRICE\n\tquantity:$QUANTITY\n\taguent:$AGENTID\n\tagent type:$AGENTTYPE\n\tunit:$UNIT" >> $LOG_FILE
+docker exec -ti cli sh -c  "peer chaincode invoke -o $ORDERER_ADDR:7050 --tls true --cafile $CA_FILE -C $CHANNEL_ID -n $CONTRACT_ID --peerAddresses $PEER1:7051 --tlsRootCertFiles $TLS_ROOT_CERT_FILES_HOUSE1 --peerAddresses $PEER2:7051 --tlsRootCertFiles $TLS_ROOT_CERT_FILES_HOUSE2  -c '{\"Args\":[\"putBid\",\"$PRICE\",\"$QUANTITY\",\"$AGENTID\",\"$AGENTTYPE\",\"$UNIT\"]}'" >> $LOG_FILE
+
+sleep 60
+
+echo -e "\n getMCP\n--------" >> $LOG_FILE
+docker exec -ti cli sh -c  "peer chaincode query -C $CHANNEL_ID -n $CONTRACT_ID -c '{\"Args\": [\"getMCP\",\"300\"]}'" >> $LOG_FILE
+# echo -e "Correct values\n\tMCP: 50\n\tBPP: 100"
+echo -e 'Correct values:\n{"MCP": 50, "BPP": 100}' >> $LOG_FILE
