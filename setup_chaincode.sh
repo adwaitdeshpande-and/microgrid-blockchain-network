9591fe9615e1e9ba1f4a36ba95d5444af4ac7483
Adwait Deshpande <71770626+adwaitdeshpande-and@users.noreply.github.com>
2024-03-26 18:05:51 +0000

    Added Chaincode setup file

---
+++ setup_chaincode.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+CONTRACT1_ID="carecords"
+CONTRACT2_ID="mcp"
+CHANNEL_ID="degrid"
+ORDERER_ADDR="orderer.microgrid.org"
+CA_FILE="/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/microgrid.org/orderers/$ORDERER_ADDR/msp/tlscacerts/tlsca.microgrid.org-cert.pem"
+
+docker exec -it cli sh -c "cd /opt/gopath/src && go mod init chaincode && go mod tidy"
+
+echo "Installing chaincode"
+
+for i in {1..6}
+do
+  HOUSE="House0"$i
+  HOUSE_ADDR=${HOUSE,,}".microgrid.org"
+  PEER_ADDR="peer0".$HOUSE_ADDR
+  CORE_PEER_LOCALMSPID=$HOUSE"MSP"
+  CORE_PEER_MSPCONFIGPATH="/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/$HOUSE_ADDR/users/Admin@$HOUSE_ADDR/msp"
+  CORE_PEER_TLS_ROOTCERT_FILE="/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/$HOUSE_ADDR/peers/$PEER_ADDR/tls/ca.crt"
+
+  docker exec -ti cli sh -c "CORE_PEER_MSPCONFIGPATH=$CORE_PEER_MSPCONFIGPATH CORE_PEER_ADDRESS=$PEER_ADDR:7051 CORE_PEER_LOCALMSPID=$CORE_PEER_LOCALMSPID CORE_PEER_TLS_ROOTCERT_FILE=$CORE_PEER_TLS_ROOTCERT_FILE peer chaincode install -n $CONTRACT1_ID -v 1.0 -p /opt/gopath/src/chaincode/$CONTRACT1_ID -l golang"
+  sleep 3s
+  docker exec -ti cli sh -c "CORE_PEER_MSPCONFIGPATH=$CORE_PEER_MSPCONFIGPATH CORE_PEER_ADDRESS=$PEER_ADDR:7051 CORE_PEER_LOCALMSPID=$CORE_PEER_LOCALMSPID CORE_PEER_TLS_ROOTCERT_FILE=$CORE_PEER_TLS_ROOTCERT_FILE peer chaincode install -n $CONTRACT2_ID -v 1.0 -p /opt/gopath/src/chaincode/$CONTRACT2_ID -l golang"
+  sleep 3s
+done
+
+
+HOUSE1="House01"
+HOUSE2="House02"
+HOUSE1_MSP=$HOUSE1"MSP"
+HOUSE2_MSP=$HOUSE2"MSP"
+
+echo "Instantiating chaincode"
+docker exec -ti cli sh -c "peer chaincode instantiate -o $ORDERER_ADDR:7050 --tls --cafile $CA_FILE -C $CHANNEL_ID -n $CONTRACT1_ID -v 1.0 -c '{\"Args\":[\"init\"]}' -P \"OR ('$HOUSE1_MSP.member', '$HOUSE2_MSP.member')\""
+sleep 3s
+docker exec -ti cli sh -c "peer chaincode instantiate -o $ORDERER_ADDR:7050 --tls --cafile $CA_FILE -C $CHANNEL_ID -n $CONTRACT2_ID -v 1.0 -c '{\"Args\":[\"init\"]}' -P \"OR ('$HOUSE1_MSP.member', '$HOUSE2_MSP.member')\""
+# docker exec -ti cli sh -c "peer chaincode instantiate -o $ORDERER_ADDR:7050 --tls --cafile $CA_FILE -C $CHANNEL_ID -n $CONTRACT1_ID -v 1.0 -c '{\"Args\":[\"init\"]}' -P \"OR ('House01.member', 'House02.member')\""
